8d5fcf3fdd37fc369e4c09f5609e957c28dddbf8
caverna2k <daniel.rubel@gmail.com>
2020-04-30 18:23:42 -0300

    First commit
    First commit, basic rest function and package structure done

---
+++ TesteBancoOriginal/src/main/java/com/original/teste/TesteBancoOriginalApplication.java
@@ -0,0 +1,13 @@
+package com.original.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBancoOriginalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBancoOriginalApplication.class, args);
+	}
+
+}
---
+++ TesteBancoOriginal/src/test/java/com/original/teste/TesteBancoOriginalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.original.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBancoOriginalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
